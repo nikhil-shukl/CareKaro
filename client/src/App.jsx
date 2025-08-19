2172aaaa5cb728576fe13f804ca280b82425ee0b
VinitKaple <vinitskaple@gmail.com>
2025-08-19 21:35:15 +0530

    clerk was setup and landing page was created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import {  Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import Landing from "./pages/Landing";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <Toaster />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
