958bb1a7e20ea4c5bf969b79ac162f29275a16fa
VinitKaple <vinitskaple@gmail.com>
2025-08-23 22:57:31 +0530

    first deployment begins

--- server/server.js
+++ server/server.js
@@ -34,6 +34,7 @@ app.use("/api/askhelp", askHelpRoutes);
 
 await connectDB();
 
-app.listen(PORT, () => {
-  console.log(`server is running on PORT: ${PORT}`);
-});
+if(process.env.NODE_ENV !== "production"){
+    const PORT = process.env.PORT || 5000;
+    server.listen(PORT, ()=> console.log("Server is running on PORT: " + PORT));
+}
